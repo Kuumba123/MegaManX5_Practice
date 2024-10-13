e1e283c65b1bf715b4056a6169fad1d8652a862d
PogChampGuy <Kuumba.Lambie@gmail.com>
2024-10-13 18:48:44 -0400

    Timer Reset Fix

--- src/skipMaverick.c
+++ src/skipMaverick.c
@@ -3,12 +3,18 @@
 
 void AssignWeapons();
 
-void SkipMaverickShowcase(Game * gameP)
+void SkipMaverickShowcase(Game *gameP)
 {
     if (fadeDirection != 0)
     {
         return;
     }
+    gameP->stageTime = 0;
+    gameP->enemiesDefeated = 0;
+    gameP->damageTaken = 0;
+    gameP->maxDamageDealt = 0;
+    gameP->virusModeCount;
+    gameP->armorPartsPast = gameP->armorParts;
     
     for (size_t i = 0; i < 16; i++)
     {
