c91a08b0cb56f88c97f8b010e6a0fb12e8cf9d0f
PogChampGuy <Kuumba.Lambie@gmail.com>
2024-10-20 18:50:25 -0400

    changed 'CONTINE' text to 'MANIP'

--- arc/write.bat
+++ arc/write.bat
@@ -1,3 +1,4 @@
 python datwrite.py "ARC_75 (ST0D_00.ARC).ARC" ROCK_X5.DAT 0x75
+python datwrite.py "ARC_76 (ST0E_00.ARC).ARC" ROCK_X5.DAT 0x76
 python datwrite.py "ARC_2E (COL16_0X.ARC).ARC" ROCK_X5.DAT 0x2E
 python datwrite.py "ARC_2F (COL16_0Z.ARC).ARC" ROCK_X5.DAT 0x2F
