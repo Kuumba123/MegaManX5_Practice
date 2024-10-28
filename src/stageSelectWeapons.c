19b58c54368fbd1e7f31bc1f5574566b08e801f8
PogChampGuy <Kuumba.Lambie@gmail.com>
2024-10-28 01:20:49 -0400

    Finished Routed

--- src/stageSelectWeapons.c
+++ src/stageSelectWeapons.c
@@ -2,35 +2,44 @@
 #include "practice.h"
 
 static uint8_t mavericksClearedTable[8] =
-    {
-        8, 9, 0x6B, 0, 0, 0, 9, 0};
-static uint8_t maverickRankTable[8] =
-    {
-        1, 0, 0, 0, 0, 0, 0, 0};
+    {8, 0x4D, 9, 0, 0x5F, 0x4F, 0xD, 0x7F};
 static uint8_t mavericksHoursTable[8] =
-    {
-        14, 24, 24, 15, 24, 24, 24, 24};
-
-static uint8_t maverickPlayerTable[8] =
-    {
-        1, 1, 1, 1, 1, 1, 1, 1};
+    {14, 9, 11, 15, 5, 8, 10, 2};
+static uint8_t maverickHealthTable[8] =
+    {32, 32, 32, 32, 36, 34, 32, 36};
 static uint8_t maverickAmmoTable[8] =
-    {
-        48, 48, 48, 48, 48, 48, 48, 48};
+    {48, 54, 50, 48, 54, 54, 52, 56};
+static uint8_t maverickStageSelectModeTable[8] =
+    {0, 5, 5, 0, 6, 5, 5, 8};
+static uint32_t maverickPartsTable[8] =
+    {0, 0x10, 0x10, 0, 0x4030, 0x10, 0x10, 0x4030};
 static uint8_t dynamoHoursTable[2] =
-    {
-        13, 10};
+    {13, 7};
 static uint8_t dynamoMaverickClearedTable[2] =
-    {
-        9, 0};
-static uint8_t dynamoRankTable[2] =
-    {
-        1, 0};
+    {9, 0x5F};
 static uint32_t dynamoPartsTable[2] =
-    {
-        0, 0x10};
+    {0, 0x4030};
+static uint8_t dynamoStageSelectModeTable[2] =
+    {0, 5};
+static uint8_t dynamoHealthTable[2] =
+    {32, 36};
+static uint8_t dynamoAmmoTable[2] =
+    {50, 48};
+static uint8_t sigmaHoursTable[3] =
+    {6, 4, 3};
+static uint8_t sigmaMaverickClearedTable[3] =
+    {0x5F, 0x7F, 0x7F};
+static uint32_t sigmaPartsTable[3] =
+    {0x4030, 0x4030, 0x4030};
+static uint8_t sigmaHealthTable[3] =
+    {36, 36, 36};
+static uint8_t sigmaAmmoTable[3] =
+    {54, 56, 56};
+
 void AssignWeapons() // 2 routes: All Stages & Any%
 {
+    uint32_t parts = 0;
+
     game.igt = 0;
     game.armorParts = 0;
     game.armors = 0;
@@ -56,45 +65,81 @@ void AssignWeapons() // 2 routes: All Stages & Any%
 
     if (game.stageId == 0) // Intro
     {
+        game.stageSelectMode = 0;
     }
     else if (game.stageId <= 8) // 8 Maverick Stages
     {
-        game.player = maverickPlayerTable[game.stageId - 1];
+        if (game.stageId != 4)
+        {
+            game.ranks[1] = 2;
+        }
         game.clearedStages = mavericksClearedTable[game.stageId - 1];
         game.hoursLeft = mavericksHoursTable[game.stageId - 1] * 3600 * 60;
-        game.ranks[game.player] = maverickRankTable[game.stageId - 1];
+        game.maxHPs[1] = maverickHealthTable[game.stageId - 1];
+        game.maxAmmos[1] = maverickAmmoTable[game.stageId - 1];
+        game.stageSelectMode = maverickStageSelectModeTable[game.stageId - 1];
+        parts = maverickPartsTable[game.stageId - 1];
     }
     else if (game.stageId == 0xC) // Final Level
     {
+        if (practice.ultimateArmor)
+        {
+            game.player = 0;
+            game.armorType = 4;
+        }
+        game.ranks[1] = 2;
+        game.stageSelectMode = 8;
         game.armors = 0x10;
         if (practice.route == 0)
         {
             game.clearedStages = 0xFF;
-            if (practice.ultimateArmor)
+            game.maxHPs[1] = 38;
+            game.maxAmmos[1] = 58;
+            
+            if (!practice.ultimateArmor)
             {
-                game.player = 0;
-                game.armorType = 4;
+                parts = 0x4030;
+            }else
+            {
+                parts = 0x10;
             }
         }
     }
     else if (game.stageId < 0xC) // dynamo fights...
     {
+        game.ranks[1] = 2;
         game.hoursLeft = dynamoHoursTable[game.stageId - 9] * 3600 * 60;
         game.clearedStages = dynamoMaverickClearedTable[game.stageId - 9];
-        game.equipedParts[5] = dynamoPartsTable[game.stageId - 9];
+        parts = dynamoPartsTable[game.stageId - 9];
+        game.stageSelectMode = dynamoStageSelectModeTable[game.stageId - 9];
+        game.maxHPs[1] = dynamoHealthTable[game.stageId - 9];
+        game.maxAmmos[1] = dynamoAmmoTable[game.stageId - 9];
     }
     else
     { // other sigma stages
+        game.ranks[1] = 2;
+        game.stageSelectMode = game.stageId - 0x10 + 5;
 
-        if (practice.route == 0)
-        {
-            game.clearedStages = 0x7F;
-        }
-
-        if (practice.ultimateArmor && game.stageId == 0x12 && practice.route != 0)
+        if (practice.ultimateArmor && game.stageId == 0x12)
         {
             game.player = 0;
             game.armorType = 0;
+        }
+
+        if (practice.route == 0)
+        {
+            game.hoursLeft = sigmaHoursTable[game.stageId - 0x10] * 3600 * 60;
+            game.clearedStages = sigmaMaverickClearedTable[game.stageId - 0x10];
+            if (game.player != 0)
+            {
+                parts = sigmaPartsTable[game.stageId - 0x10];
+            }else
+            {
+                parts = 0x10;
+            }
+
+            game.maxHPs[1] = sigmaHealthTable[game.stageId - 0x10];
+            game.maxAmmos[1] = sigmaAmmoTable[game.stageId - 0x10];
         }
     }
 
@@ -103,6 +148,7 @@ void AssignWeapons() // 2 routes: All Stages & Any%
         game.collectables = 0x1000;
         game.tanksAmmo[0] = 12;
     }
+    game.equipedParts[game.armorType] = parts;
 }
 
 void ExitRoutePage(Game *gameP)
